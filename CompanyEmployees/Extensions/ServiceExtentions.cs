5fe05cb18f008924fe02201b411cc616613a05d9
Ammar Mahmoud <ammar.m777.2@gmail.com>
2024-01-21 18:30:11 +0200

    Creating project and add basic configuration

---
+++ CompanyEmployees/Extensions/ServiceExtentions.cs
@@ -0,0 +1,18 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //cors in mandatory to send requests from a different domain to our application
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            { 
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin() // to allow requests form indicated source not all sources use [WithOrigins("https://example.com")] 
+                .AllowAnyMethod() // u can use [WithMethods("POST", "GET")] to allow these to method 
+                .AllowAnyHeader()); //the WithHeaders("accept", "content-type") method to allow only specific headers.
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(option => { });
+    }
+}
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,14 +1,38 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();//adds HTTP Strict Transport Security (HSTS) headers to the responses,
+                  //instructing browsers to enforce secure connections over HTTPS.
+}
+
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles(); // if u did not use it will use a wwwroot folder in our project by default.
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All 
+}); 
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
